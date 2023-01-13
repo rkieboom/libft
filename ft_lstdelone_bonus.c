26963cc84325f7ea3cac6e4805c277fe763d759a
rkieboom <rowankieboom015@gmail.com>
2023-01-13 12:35:37 +0100

    Final

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstdelone_bonus.c                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/15 12:46:46 by rkieboom       #+#    #+#                */
+/*   Updated: 2019/11/29 07:24:22 by rkieboom      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	del(lst->content);
+	free(lst);
+}
---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstlast.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/13 14:33:15 by rkieboom       #+#    #+#                */
+/*   Updated: 2019/11/15 17:21:59 by rkieboom      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
