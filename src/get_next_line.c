149db3ebf0c6e2b0354e3b0f59e39eff81cd7db5
rkieboom <rkieboom@student.codam.nl>
2023-03-15 02:26:03 +0100

    Better gnl

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -1,39 +1,149 @@
 #include "libft.h"
 
-char *get_next_line(int fd)
+
+
+size_t	gnl_strlen(char *str)
 {
-	static char	*line;
-	char	*buffer;
+	size_t	c;
+
+	c = 0;
+	if (!str)
+		return (0);
+	while (str[c] != '\0')
+		c++;
+	return (c);
+}
+
+char	*gnl_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[gnl_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*gnl_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	c;
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc((gnl_strlen(s1) + gnl_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	c = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[c] != '\0')
+		str[i++] = s2[c++];
+	str[gnl_strlen(s1) + gnl_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_get_line(char *save)
+{
+	int		i;
+	char	*s;
+
+	i = 0;
+	if (!save[i])
+		return (NULL);
+	while (save[i] && save[i] != '\n')
+		i++;
+	s = (char *)malloc(sizeof(char) * (i + 1));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+	{
+		s[i] = save[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
+char	*ft_save(char *save)
+{
+	int		i;
+	int		c;
+	char	*s;
+
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (NULL);
+	}
+	s = (char *)malloc(sizeof(char) * (gnl_strlen(save) - i + 1));
+	if (!s)
+		return (NULL);
+	i++;
+	c = 0;
+	while (save[i])
+		s[c++] = save[i++];
+	s[c] = '\0';
+	free(save);
+	return (s);
+}
+
+char	*ft_read_and_save(int fd, char *save)
+{
+	char	*buff;
 	int		read_bytes;
-	char	*temp;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	read_bytes = 1;
+	while (!gnl_strchr(save, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buff, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[read_bytes] = '\0';
+		save = gnl_strjoin(save, buff);
+	}
+	free(buff);
+	return (save);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*save;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	save = ft_read_and_save(fd, save);
+	if (!save)
 		return (NULL);
-	if (line == NULL)
-		line = ft_calloc(1, sizeof(char));
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (buffer == NULL || line == NULL)
-		return (free(line), NULL);
-	read_bytes = read(fd, buffer, BUFFER_SIZE);
-	while (read_bytes > 0)
-	{
-		if (read_bytes == -1)
-			return (free(line), free(buffer), NULL);
-		else if (read_bytes == 0)
-			break ;
-		buffer[read_bytes] = '\0';
-		temp = ft_strjoin(line, buffer);
-		free(line);
-		line = temp;
-		if (line == NULL)
-			return (NULL);
-		if (ft_strchr(buffer, '\n') != NULL)
-			break ;
-		read_bytes = read(fd, buffer, BUFFER_SIZE);
-
-	}
-	free(buffer);
-	if (line[0] == '\0')
-		return (free(line), NULL);
+	line = ft_get_line(save);
+	save = ft_save(save);
 	return (line);
 }
